93c4ac89b7be48f7b5997e4afc6fe294ee95b6bd
Sudatra Ghosh <sudatraghosh@jmangroup.com>
2025-02-27 12:19:35 +0530

    added schemas routes and services for orders application

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { OrdersModule } from './orders.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
-  await app.listen(process.env.port ?? 3000);
+  const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -13,7 +13,8 @@ import { Order, OrderSchema } from './schemas/order.schema';
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
-        MONGODB_URI: Joi.string().required()
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required()
       }),
       envFilePath: './apps/orders/.env'
     }),
