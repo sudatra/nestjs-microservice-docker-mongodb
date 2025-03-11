676edbd81c56c884975458ce10571805dc786897
sudatra <mail2sudatra@gmail.com>
2025-03-11 20:24:01 +0530

    added billing microservice event emit pattern for order creation

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -7,6 +7,7 @@ import { DatabaseModule, RmqModule } from '@app/common';
 import { OrdersRepository } from './orders.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './schemas/order.schema';
+import { BILLING_SERVICE } from './constants/services';
 
 @Module({
   imports: [
@@ -20,7 +21,9 @@ import { Order, OrderSchema } from './schemas/order.schema';
     }),
     DatabaseModule,
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
-    // RmqModule.register({ })
+    RmqModule.register({
+      name: BILLING_SERVICE
+    })
   ],
   controllers: [OrdersController],
   providers: [OrdersService, OrdersRepository],
